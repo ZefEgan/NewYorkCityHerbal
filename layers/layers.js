ed8a4c9c32cf75764756b735df50581177d72eb2
ZefEgan <Zefegan@gmail.com>
2026-08-02 10:57:20 -0400

    first commit

---
+++ layers/layers.js
@@ -0,0 +1,134 @@
+var wms_layers = [];
+
+var format_Boroughs_0 = new ol.format.GeoJSON();
+var features_Boroughs_0 = format_Boroughs_0.readFeatures(json_Boroughs_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Boroughs_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Boroughs_0.addFeatures(features_Boroughs_0);
+var lyr_Boroughs_0 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Boroughs_0, 
+                style: style_Boroughs_0,
+                popuplayertitle: 'Boroughs',
+                interactive: false,
+                title: '<img src="styles/legend/Boroughs_0.png" /> Boroughs'
+            });
+var format_Cemeteries_1 = new ol.format.GeoJSON();
+var features_Cemeteries_1 = format_Cemeteries_1.readFeatures(json_Cemeteries_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Cemeteries_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cemeteries_1.addFeatures(features_Cemeteries_1);
+var lyr_Cemeteries_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Cemeteries_1, 
+                style: style_Cemeteries_1,
+                popuplayertitle: 'Cemeteries',
+                interactive: true,
+                title: '<img src="styles/legend/Cemeteries_1.png" /> Cemeteries'
+            });
+var format_ProspectPark_2 = new ol.format.GeoJSON();
+var features_ProspectPark_2 = format_ProspectPark_2.readFeatures(json_ProspectPark_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ProspectPark_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ProspectPark_2.addFeatures(features_ProspectPark_2);
+var lyr_ProspectPark_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_ProspectPark_2, 
+                style: style_ProspectPark_2,
+                popuplayertitle: 'Prospect Park',
+                interactive: false,
+                title: '<img src="styles/legend/ProspectPark_2.png" /> Prospect Park'
+            });
+var format_Parks_3 = new ol.format.GeoJSON();
+var features_Parks_3 = format_Parks_3.readFeatures(json_Parks_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Parks_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Parks_3.addFeatures(features_Parks_3);
+var lyr_Parks_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Parks_3, 
+                style: style_Parks_3,
+                popuplayertitle: 'Parks',
+                interactive: true,
+                title: '<img src="styles/legend/Parks_3.png" /> Parks'
+            });
+var format_MajorRoads_4 = new ol.format.GeoJSON();
+var features_MajorRoads_4 = format_MajorRoads_4.readFeatures(json_MajorRoads_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_MajorRoads_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_MajorRoads_4.addFeatures(features_MajorRoads_4);
+var lyr_MajorRoads_4 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_MajorRoads_4, 
+                style: style_MajorRoads_4,
+                popuplayertitle: 'Major Roads',
+                interactive: false,
+                title: '<img src="styles/legend/MajorRoads_4.png" /> Major Roads'
+            });
+var format_Waterfeature_5 = new ol.format.GeoJSON();
+var features_Waterfeature_5 = format_Waterfeature_5.readFeatures(json_Waterfeature_5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Waterfeature_5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Waterfeature_5.addFeatures(features_Waterfeature_5);
+var lyr_Waterfeature_5 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Waterfeature_5, 
+                style: style_Waterfeature_5,
+                popuplayertitle: 'Water feature',
+                interactive: false,
+                title: '<img src="styles/legend/Waterfeature_5.png" /> Water feature'
+            });
+var format_FlorareferencedbyDioscorides_6 = new ol.format.GeoJSON();
+var features_FlorareferencedbyDioscorides_6 = format_FlorareferencedbyDioscorides_6.readFeatures(json_FlorareferencedbyDioscorides_6, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_FlorareferencedbyDioscorides_6 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_FlorareferencedbyDioscorides_6.addFeatures(features_FlorareferencedbyDioscorides_6);
+var lyr_FlorareferencedbyDioscorides_6 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_FlorareferencedbyDioscorides_6, 
+                style: style_FlorareferencedbyDioscorides_6,
+                popuplayertitle: 'Flora referenced by Dioscorides',
+                interactive: true,
+                title: '<img src="styles/legend/FlorareferencedbyDioscorides_6.png" /> Flora referenced by Dioscorides'
+            });
+
+lyr_Boroughs_0.setVisible(true);lyr_Cemeteries_1.setVisible(true);lyr_ProspectPark_2.setVisible(true);lyr_Parks_3.setVisible(true);lyr_MajorRoads_4.setVisible(true);lyr_Waterfeature_5.setVisible(true);lyr_FlorareferencedbyDioscorides_6.setVisible(true);
+var layersList = [lyr_Boroughs_0,lyr_Cemeteries_1,lyr_ProspectPark_2,lyr_Parks_3,lyr_MajorRoads_4,lyr_Waterfeature_5,lyr_FlorareferencedbyDioscorides_6];
+lyr_Boroughs_0.set('fieldAliases', {'BoroCode': 'BoroCode', 'BoroName': 'BoroName', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', });
+lyr_Cemeteries_1.set('fieldAliases', {'borocode': 'borocode', 'boroname': 'boroname', 'countyfips': 'countyfips', 'nta2020': 'nta2020', 'ntaname': 'ntaname', 'ntaabbrev': 'ntaabbrev', 'ntatype': 'ntatype', 'cdta2020': 'cdta2020', 'cdtaname': 'cdtaname', 'shape_leng': 'shape_leng', 'shape_area': 'shape_area', });
+lyr_ProspectPark_2.set('fieldAliases', {'date_acqui': 'date_acqui', 'time_acqui': 'time_acqui', 'acres': 'acres', 'address': 'address', 'borough': 'borough', 'class': 'class', 'communityb': 'communityb', 'councildis': 'councildis', 'department': 'department', 'eapply': 'eapply', 'gisobjid': 'gisobjid', 'gispropnum': 'gispropnum', 'globalid': 'globalid', 'jurisdicti': 'jurisdicti', 'location': 'location', 'mapped': 'mapped', 'name311': 'name311', 'nys_assemb': 'nys_assemb', 'nys_senate': 'nys_senate', 'objectid': 'objectid', 'omppropid': 'omppropid', 'parentid': 'parentid', 'permit': 'permit', 'permitdist': 'permitdist', 'permitpare': 'permitpare', 'pip_ratabl': 'pip_ratabl', 'precinct': 'precinct', 'retired': 'retired', 'signname': 'signname', 'subcategor': 'subcategor', 'typecatego': 'typecatego', 'us_congres': 'us_congres', 'waterfront': 'waterfront', 'zipcode': 'zipcode', });
+lyr_Parks_3.set('fieldAliases', {'borough': 'borough', 'communityb': 'communityb', 'councildis': 'councildis', 'gispropnum': 'gispropnum', 'parkdistri': 'parkdistri', 'precinct': 'precinct', 'propertyna': 'propertyna', 'zipcode': 'zipcode', });
+lyr_MajorRoads_4.set('fieldAliases', {'Borough': 'Borough', 'Boro_Code': 'Boro_Code', 'Route_Name': 'Route_Name', 'Route_Type': 'Route_Type', 'Route_Sub': 'Route_Sub', 'Route_Stat': 'Route_Stat', });
+lyr_Waterfeature_5.set('fieldAliases', {'gispropnum': 'gispropnum', 'objectid': 'objectid', 'department': 'department', 'parentid': 'parentid', 'communityb': 'communityb', 'councildis': 'councildis', 'precinct': 'precinct', 'zipcode': 'zipcode', 'borough': 'borough', 'name': 'name', 'wf_type': 'wf_type', 'featuresta': 'featuresta', });
+lyr_FlorareferencedbyDioscorides_6.set('fieldAliases', {'observed_o': 'observed_o', 'url': 'url', 'descriptio': 'descriptio', 'latitude': 'latitude', 'longitude': 'longitude', 'scientific': 'scientific', 'common_nam': 'common_nam', 'BoroName': 'BoroName', 'propertyna': 'propertyna', 'botan_illu': 'botan_illu', 'layer': 'layer', });
+lyr_Boroughs_0.set('fieldImages', {'BoroCode': 'Range', 'BoroName': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', });
+lyr_Cemeteries_1.set('fieldImages', {'borocode': 'TextEdit', 'boroname': 'TextEdit', 'countyfips': 'TextEdit', 'nta2020': 'TextEdit', 'ntaname': 'TextEdit', 'ntaabbrev': 'TextEdit', 'ntatype': 'TextEdit', 'cdta2020': 'TextEdit', 'cdtaname': 'TextEdit', 'shape_leng': 'TextEdit', 'shape_area': 'TextEdit', });
+lyr_ProspectPark_2.set('fieldImages', {'date_acqui': 'DateTime', 'time_acqui': 'TextEdit', 'acres': 'TextEdit', 'address': 'TextEdit', 'borough': 'TextEdit', 'class': 'TextEdit', 'communityb': 'TextEdit', 'councildis': 'TextEdit', 'department': 'TextEdit', 'eapply': 'TextEdit', 'gisobjid': 'TextEdit', 'gispropnum': 'TextEdit', 'globalid': 'TextEdit', 'jurisdicti': 'TextEdit', 'location': 'TextEdit', 'mapped': 'TextEdit', 'name311': 'TextEdit', 'nys_assemb': 'TextEdit', 'nys_senate': 'TextEdit', 'objectid': 'TextEdit', 'omppropid': 'TextEdit', 'parentid': 'TextEdit', 'permit': 'TextEdit', 'permitdist': 'TextEdit', 'permitpare': 'TextEdit', 'pip_ratabl': 'TextEdit', 'precinct': 'TextEdit', 'retired': 'TextEdit', 'signname': 'TextEdit', 'subcategor': 'TextEdit', 'typecatego': 'TextEdit', 'us_congres': 'TextEdit', 'waterfront': 'TextEdit', 'zipcode': 'TextEdit', });
+lyr_Parks_3.set('fieldImages', {'borough': 'TextEdit', 'communityb': 'TextEdit', 'councildis': 'TextEdit', 'gispropnum': 'TextEdit', 'parkdistri': 'TextEdit', 'precinct': 'TextEdit', 'propertyna': 'TextEdit', 'zipcode': 'TextEdit', });
+lyr_MajorRoads_4.set('fieldImages', {'Borough': 'TextEdit', 'Boro_Code': 'Range', 'Route_Name': 'TextEdit', 'Route_Type': 'TextEdit', 'Route_Sub': 'TextEdit', 'Route_Stat': 'TextEdit', });
+lyr_Waterfeature_5.set('fieldImages', {'gispropnum': 'TextEdit', 'objectid': 'TextEdit', 'department': 'TextEdit', 'parentid': 'TextEdit', 'communityb': 'TextEdit', 'councildis': 'TextEdit', 'precinct': 'TextEdit', 'zipcode': 'TextEdit', 'borough': 'TextEdit', 'name': 'TextEdit', 'wf_type': 'TextEdit', 'featuresta': 'TextEdit', });
+lyr_FlorareferencedbyDioscorides_6.set('fieldImages', {'observed_o': 'DateTime', 'url': 'TextEdit', 'descriptio': 'TextEdit', 'latitude': 'TextEdit', 'longitude': 'TextEdit', 'scientific': 'TextEdit', 'common_nam': 'TextEdit', 'BoroName': 'TextEdit', 'propertyna': 'TextEdit', 'botan_illu': 'TextEdit', 'layer': 'TextEdit', });
+lyr_Boroughs_0.set('fieldLabels', {'BoroCode': 'no label', 'BoroName': 'no label', 'Shape_Leng': 'no label', 'Shape_Area': 'no label', });
+lyr_Cemeteries_1.set('fieldLabels', {'borocode': 'hidden field', 'boroname': 'hidden field', 'countyfips': 'hidden field', 'nta2020': 'hidden field', 'ntaname': 'no label', 'ntaabbrev': 'hidden field', 'ntatype': 'hidden field', 'cdta2020': 'hidden field', 'cdtaname': 'hidden field', 'shape_leng': 'hidden field', 'shape_area': 'hidden field', });
+lyr_ProspectPark_2.set('fieldLabels', {'date_acqui': 'no label', 'time_acqui': 'no label', 'acres': 'no label', 'address': 'no label', 'borough': 'no label', 'class': 'no label', 'communityb': 'no label', 'councildis': 'no label', 'department': 'no label', 'eapply': 'no label', 'gisobjid': 'no label', 'gispropnum': 'no label', 'globalid': 'no label', 'jurisdicti': 'no label', 'location': 'no label', 'mapped': 'no label', 'name311': 'no label', 'nys_assemb': 'no label', 'nys_senate': 'no label', 'objectid': 'no label', 'omppropid': 'no label', 'parentid': 'no label', 'permit': 'no label', 'permitdist': 'no label', 'permitpare': 'no label', 'pip_ratabl': 'no label', 'precinct': 'no label', 'retired': 'no label', 'signname': 'no label', 'subcategor': 'no label', 'typecatego': 'no label', 'us_congres': 'no label', 'waterfront': 'no label', 'zipcode': 'no label', });
+lyr_Parks_3.set('fieldLabels', {'borough': 'hidden field', 'communityb': 'hidden field', 'councildis': 'hidden field', 'gispropnum': 'hidden field', 'parkdistri': 'hidden field', 'precinct': 'hidden field', 'propertyna': 'no label', 'zipcode': 'hidden field', });
+lyr_MajorRoads_4.set('fieldLabels', {'Borough': 'no label', 'Boro_Code': 'no label', 'Route_Name': 'no label', 'Route_Type': 'no label', 'Route_Sub': 'no label', 'Route_Stat': 'no label', });
+lyr_Waterfeature_5.set('fieldLabels', {'gispropnum': 'no label', 'objectid': 'no label', 'department': 'no label', 'parentid': 'no label', 'communityb': 'no label', 'councildis': 'no label', 'precinct': 'no label', 'zipcode': 'no label', 'borough': 'no label', 'name': 'no label', 'wf_type': 'no label', 'featuresta': 'no label', });
+lyr_FlorareferencedbyDioscorides_6.set('fieldLabels', {'observed_o': 'hidden field', 'url': 'no label', 'descriptio': 'no label', 'latitude': 'hidden field', 'longitude': 'hidden field', 'scientific': 'no label', 'common_nam': 'no label', 'BoroName': 'hidden field', 'propertyna': 'hidden field', 'botan_illu': 'no label', 'layer': 'hidden field', });
+lyr_FlorareferencedbyDioscorides_6.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/FlorareferencedbyDioscorides_6_style.js
@@ -0,0 +1,34 @@
+var size = 0;
+var placement = 'point';
+
+var style_FlorareferencedbyDioscorides_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 2.0000000000000004 + size,
+            displacement: [0, 0],  fill: new ol.style.Fill({color: 'rgba(124,168,157,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
